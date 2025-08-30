be4d4a184ea78bc8775516c25c969d1ee03e18f0
YaOgreMaga0 <con15.11.2007@gmail.com>
2025-08-30 12:23:28 +0300

    документация

--- src/MyASSerts.cpp
+++ src/MyASSerts.cpp
@@ -5,13 +5,17 @@
 #include <math.h>
 
 
-
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+//! checks the condition for the correct launch of the function
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
 void ASS(const char *func, const char *FILE, int LINE, bool condition)
 {
     if(!condition)
         printf(RED "\n MyASSertion error!\n  In file  %s\n In function  %s\n In line  %d\n" BLACK, func, FILE, LINE);
 }
-
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
+//! checks a number for finiteness
+//‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐
 bool IsCertain(int a)
 {
     if(a == NAN)
